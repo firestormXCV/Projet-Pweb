0d09be01627f411daf19a8ae174e4c7ea5ab62db
TBrioche <72711057+TBrioche@users.noreply.github.com>
2021-10-15 16:56:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php>
+<?php 
+
+if (isset($_GET['controle']) & isset($_GET['action'])) {
+     $controle = $_GET['controle'];
+    $action= $_GET['action'];
+    }
+else { //absence de paramètres : prévoir des valeurs par défaut
+    $controle = "voiture";
+    $action= "listeVoiture";
+    }
+
+//inclure le fichier php de contrôle 
+//et lancer la fonction-action issue de ce fichier.
+
+    require ('./controle/' . $controle . '.php');
+    $action (); 
+
+?>
