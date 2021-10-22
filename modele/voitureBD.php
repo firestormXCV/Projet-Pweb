2fc6d1ecac11d8d7bdff1c8138887e3e502343a1
Tracyhong <72313520+Tracyhong@users.noreply.github.com>
2021-10-22 15:59:28 +0200

    afficheVoiture

---
+++ modele/voitureBD.php
@@ -0,0 +1,32 @@
+<?php
+
+
+    function afficheVoiture(&$voitures){
+        require('./modele/connectBD.php');
+        $sql="SELECT * FROM voiture";
+        try{
+            $commande = $pdo->prepare($sql);
+			$bool = $commande->execute();
+			if ($bool) {
+				$resultat = $commande->fetchAll(PDO::FETCH_ASSOC); //tableau d'enregistrements
+				//var_dump($resultat); die();
+				/*while ($ligne = $commande->fetch()) { // ligne par ligne
+					print_r($ligne);
+				}*/
+			}
+        }
+        catch (PDOException $e) {
+			echo utf8_encode("Echec de SQL : " . $e->getMessage() . "\n");
+			die(); // On arrête tout.
+		}
+        if (count($resultat) == 0) {
+			$voitures=array(); 
+			return false; 
+		}
+		else {
+			$voitures = $resultat;
+			//var_dump($profil);
+			return true;
+		}
+    }
+?>
--- vue/voiture/listeVoiture.tpl
+++ vue/voiture/listeVoiture.tpl
@@ -1,2 +1,13 @@
 <h2>Liste de nos voitures !</h2>
 <!--Recuperer les voitures de la base-->
+<?php 
+
+    foreach ($_SESSION['voitures'] as $value)
+        //a continuer
+
+
+
+?>
+
+<div id ="m"> <?php echo $msg; ?> </div>
+
