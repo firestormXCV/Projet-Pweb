513422910332e23e025025ff06101bf330c30840
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2021-11-03 18:13:58 +0100

    ....

--- controle/inscriptionClient.php
+++ controle/inscriptionClient.php
@@ -1,14 +1,17 @@
 <?php
 function inscription(){
     require("./Formulaire.php");
-    require("./reponse.php");
     require("./modele/inscriptionClient.php");
     $msg="";
     if (!isset($_GET['nom']) && !isset($_GET['email']) && !isset($_GET['mdp'])){
-        $msg = "Remplir tous les champs";
-        print($msg);
+        $msg = "Veuillez remplir tous les champs";
+    } else {
+        $bool = IsClient($_GET['nom'], $_GET['email']);
+        if ($bool == false){
+            inscriptionClient($_GET['nom'], $_GET['mdp'], $_GET['email']);
+        } else {
+            $msg = "Ce client existe déjà";
+        }
     }
-    
-
 }
 ?>
